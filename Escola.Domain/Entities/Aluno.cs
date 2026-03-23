fa6bebddae8b6f66b2a8e10805b7be3b4aa1831e
Leonardo Horta <leobrhorta@gmail.com>
2026-03-23 10:28:30 -0300

    Add project files.

---
+++ Escola.Domain/Entities/Aluno.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escola.Domain.Entities
+{
+    public class Aluno
+    {
+
+        public Aluno()
+        {
+            AlunoId = Guid.NewGuid();
+            NomeAluno = String.Empty;
+            Email = String.Empty;
+        }
+
+        public int Id { get; set; }
+        public Guid AlunoId { get; set; }
+        public string NomeAluno { get; set; }
+
+        public string Email { get; set; }
+
+        public Escola Escola { get; set; }  
+    }
+}
---
+++ Escola.Domain/Entities/Escola.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escola.Domain.Entities
+{
+    public class Escola
+    {
+
+        public Escola()
+        {
+            NomeEscola = String.Empty;
+        }
+        public int Id { get; set; }
+
+        public Guid EscolaId { get; set; }
+
+        public string NomeEscola { get; set; }
+    
+
+    }
+}
